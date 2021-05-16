9226049ad7271499a97e2f295356dd8401ee3322
Vignesh Hari <vichuhari100@gmail.com>
2021-05-16 18:23:41 +0530

    Admin Housekeeping

--- life/app/admin.py
+++ life/app/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
 from life.app.models import Job
+from django_celery_beat.models import SolarSchedule, PeriodicTask, IntervalSchedule, CrontabSchedule, ClockedSchedule
+from allauth.account.models import EmailAddress
+from django_rest_passwordreset.models import ResetPasswordToken
+
+admin.site.site_header = "Life Administration"
+admin.site.site_title = "Coronasafe Network"
+admin.site.index_title = "Life"
+
+for model in [
+    SolarSchedule,
+    PeriodicTask,
+    IntervalSchedule,
+    CrontabSchedule,
+    ClockedSchedule,
+    EmailAddress,
+    ResetPasswordToken,
+]:
+    admin.site.unregister(model)
 
 
 class JobAdmin(admin.ModelAdmin):
