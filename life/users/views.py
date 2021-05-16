bba441d49c57513f605801e1ebc2d21e5b75fac8
Vignesh Hari <vichuhari100@gmail.com>
2021-05-16 18:31:48 +0530

    Fixed Bugs

--- life/users/views.py
+++ life/users/views.py
@@ -17,34 +17,6 @@ def home_view(request):
     return render(request, "home.html")
 
 
-class SignupView(View):
-    form_class = CustomSignupForm
-    template = "users/signup.html"
-
-    def get(self, request, **kwargs):
-        try:
-            form = self.form_class()
-            if kwargs["type"] != User.TYPE_VALUE_MAP["Volunteer"]:
-                form.fields["skill"].widget = forms.HiddenInput()
-            return render(request, self.template, {"form": form, "type": kwargs["name"]})
-        except Exception as e:
-            print(e)
-            logging.error(e)
-            return HttpResponseRedirect("/500")
-
-    def post(self, request, **kwargs):
-        form = CustomSignupForm(request.POST)
-        if form.is_valid():
-            user_obj = form.save(commit=False)
-            user_obj.save()
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password1")
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect("home")
-        return render(request, self.template, {"form": form})
-
-
 class SigninView(View):
     form_class = AuthenticationForm
     template = "users/login.html"
